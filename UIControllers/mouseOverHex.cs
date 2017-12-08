8183ad86c03c46efbb0427cff154afa535702d73
TomasiewiczW <wojtarek@vp.pl>
2017-12-08 22:36:07 +0100

    bugfix
    Firstly I've fixed bug with mulligan - I'd been changing color of hex by component that hadn't exist. That had been making them in instantiates. Secondly I've added hexes after mulligan to deck list. Before they hadn't been connected with list.

--- UIControllers/mouseOverHex.cs
+++ UIControllers/mouseOverHex.cs
@@ -9,14 +9,14 @@ public class mouseOverHex : MonoBehaviour {
     void OnMouseEnter()
     {
         mouseOver = true;
-        GetComponent<Renderer>().material.SetColor("_Color", Color.black);
+        GetComponent<SpriteRenderer>().color = Color.black;
         GetComponent<Renderer>().transform.localScale = new Vector3 (0.7f, 0.7f, 1);
     }
     
     void OnMouseExit ()
     {
         mouseOver = false;
-        GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        GetComponent<SpriteRenderer>().color = Color.green;
         GetComponent<Renderer>().transform.localScale = new Vector3(0.5f, 0.5f, 1);
     }    
 
