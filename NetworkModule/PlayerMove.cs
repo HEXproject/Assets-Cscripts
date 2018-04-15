8f9eece3c58f6bcbc596b80a42e2de9fa38fe11d
arkadiuszch <arkadiusz.cholewaa@gmail.com>
2018-04-15 18:00:06 +0200

    client/host test + gitignore

---
+++ NetworkModule/PlayerMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
