d0a60036b71acba013e60fb6412bd42602a27ae7
Mrozolga <m.olgaa97@gmail.com>
2018-04-25 22:12:31 +0200

    adding Draggable class

---
+++ UIControllers/Click.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Click : MonoBehaviour {
+
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("KLIK!");
+	}
+}
