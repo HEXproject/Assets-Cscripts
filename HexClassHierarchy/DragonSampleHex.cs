d492875a5d39d438cb25e669d9a223f5a77a2e44
Wojciech Tomasiewicz <wojtarek@vp.pl>
2018-06-01 11:54:19 +0200

    test change

---
+++ HexClassHierarchy/DragonSampleHex.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.HexClassHierarchy
+{
+    public class DragonSampleHex : UnitHex
+    {
+        public override void InitHex()
+        {
+            this.HexCost = new HexCost(0, 1, 1, 2);
+            this.AttackPoints = 4;
+            this.LifePoints = 10;
+        }
+        public override void EnterOnSpecifiedTile(GameObject tile)
+        {
+
+        }
+        public override void Attack(GameObject target)
+        {
+            target.GetComponent<UnitHex>().LifePoints -= AttackPoints;
+        }
+
+        public override void Die()
+        {
+        
+        }
+    }
+}
